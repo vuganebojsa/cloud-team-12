acb4452640fe1a606c217ba96cdfc5194ef259e5
nebojsavuga <vuga.sv53.2020@uns.ac.rs>
2023-04-17 16:44:16 +0200

    Added registration endpoint

--- cloud/src/app/services/user.service.ts
+++ cloud/src/app/services/user.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '../models/user';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  register(user: User) : Observable<void> {
+    return this.http.post<void>('registracija ', user)
+  }
 }
