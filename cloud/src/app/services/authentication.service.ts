1e973c8bdf4eb3e8b609cc52f74afb02fbe26327
nebojsavuga <vuga.sv53.2020@uns.ac.rs>
2023-04-17 16:42:25 +0200

    Added necessary libraries for authentication

--- cloud/src/app/services/authentication.service.ts
+++ cloud/src/app/services/authentication.service.ts
@@ -1,17 +1,52 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Token } from '../models/token';
+import { JwtHelperService } from '@auth0/angular-jwt';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  private headers = new HttpHeaders({
+    'Content-Type': 'application/json',
+    skip:'true',
+  });
 
-  login(email: string, password: string) {
-    return null
+   user$ = new BehaviorSubject(null);
+  userState$ = this.user$.asObservable();
+
+  constructor(private http:HttpClient) {
+    this.user$.next(this.getRole());
+   }
+
+   login(email:string, password:string):Observable<Token>{
+    return this.http.post<Token>('adresa cognita', {email: email, password: password}, {
+      headers:this.headers,
+    });
+
   }
 
-  setUser() {
+  logout():void{
+    localStorage.removeItem('user');
+  }
 
+  isLoggedIn(): boolean{
+    return localStorage.getItem('user') != null;
+  }
+
+  getRole():any{
+    if(this.isLoggedIn()){
+      const accessToken: string = localStorage.getItem('user');
+      const helper = new JwtHelperService();
+
+      const role = helper.decodeToken(accessToken).role[0].authority;
+      return role;
+    }
+    return null;
+  }
+
+  setUser(): void{
+    this.user$.next(this.getRole());
   }
 }
