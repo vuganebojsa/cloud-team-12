cef1826698178da231a92293ed3cc7bd0808d286
nebojsavuga <vuga.sv53.2020@uns.ac.rs>
2023-04-17 15:26:21 +0200

    Created skeleton components

--- cloud/src/app/app.module.ts
+++ cloud/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './layout/login/login.component';
+import { RegisterComponent } from './layout/register/register.component';
+import { UploadFileComponent } from './layout/upload-file/upload-file.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    UploadFileComponent
   ],
   imports: [
     BrowserModule,
