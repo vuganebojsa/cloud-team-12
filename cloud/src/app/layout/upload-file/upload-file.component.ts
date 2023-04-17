9c41f1fc53a9fdffcee9111ca6724922d5c15b36
nebojsavuga <vuga.sv53.2020@uns.ac.rs>
2023-04-17 16:25:04 +0200

    Added desing for uploading

--- cloud/src/app/layout/upload-file/upload-file.component.ts
+++ cloud/src/app/layout/upload-file/upload-file.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-upload-file',
@@ -6,5 +9,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./upload-file.component.css']
 })
 export class UploadFileComponent {
+  uploadForm = new FormGroup({
+    filename: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    description: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    tags: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    type: new FormControl('', [Validators.required]),
+    size: new FormControl('', [Validators.required]),
+    createdAt: new FormControl('', [Validators.required]),
+    lastModifiedAt: new FormControl('', [Validators.required]),
+  });
 
+  file: string = '';
+  constructor( private userService: UserService, private router: Router){
+
+  }
+
+
+  upload():void{
+     if(this.uploadForm.valid){
+      alert("Successfully uploaded a file!");
+    }
+
+  }
+
+  handleUpload(event):void{
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+        this.file = reader.result.toString();
+    };
+    const name = file.name;
+    const type = file.type;
+    const size = Number(file.size)/1024;
+    const modifiedAt = file.lastModifiedDate;
+    const createdAt = null;
+    this.uploadForm.setValue({
+      filename: name,
+      description: this.uploadForm.value.description,
+      tags: this.uploadForm.value.tags,
+      lastModifiedAt: modifiedAt,
+      size: String(Math.round((size + Number.EPSILON) * 100) / 100
+      ),
+      type : type,
+      createdAt: createdAt
+    })
+
+  }
 }
