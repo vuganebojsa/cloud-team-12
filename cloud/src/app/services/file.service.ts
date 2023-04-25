1669eb1a5885b52eb84814cfaa441414c1a2acd6
nebojsavuga <vuga.sv53.2020@uns.ac.rs>
2023-04-25 17:39:22 +0200

    Modified file service so that it contains folders

--- cloud/src/app/services/file.service.ts
+++ cloud/src/app/services/file.service.ts
@@ -9,15 +9,26 @@ import { Observable } from 'rxjs';
 export class FileService {
 
   s3_bucket_path: string = 'https://cngfolor3e.execute-api.eu-central-1.amazonaws.com/dev/bivuja-bucket/';
+  private params = {
+    TableName: 'Files'
+  };
+  private dynamodb;
+  private docClient;
+  constructor(private http: HttpClient) { 
 
-  constructor(private http: HttpClient) { }
+    this.dynamodb = new AWS.DynamoDB();
+    this.docClient = new AWS.DynamoDB.DocumentClient();
+  }
 
 
   uploadFile(fileInfo: FileInfo, file: any): Observable<any>{
-    let headers = new HttpHeaders();
-    headers.append('Content-Type', fileInfo.type);
-    let options = { headers: headers };
-    console.log(fileInfo);
-    return this.http.put<any>(this.s3_bucket_path + fileInfo.filename, file, options);
+    let type: string = '';
+    if(fileInfo.type.toLowerCase().includes("image")) type = 'photos';
+    else if(fileInfo.type.toLowerCase().includes("text")) type = 'text';
+    else if(fileInfo.type.toLowerCase().includes("audio")) type = 'audio';
+    else if(fileInfo.type.toLowerCase().includes("video")) type = 'videos';
+    else type = 'other';
+
+    return this.http.put<any>(this.s3_bucket_path + type + "/" + fileInfo.filename, file);
   }
 }
