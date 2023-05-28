13523f1da855769815df285eaab3a28fdebe7855
nebojsavuga <vuga.sv53.2020@uns.ac.rs>
2023-05-28 15:46:22 +0200

    Added directory management

--- cloud/src/app/layout/files-display/files-display.component.ts
+++ cloud/src/app/layout/files-display/files-display.component.ts
@@ -10,37 +10,66 @@ import { FileService } from 'src/app/services/file.service';
 export class FilesDisplayComponent implements OnInit{
   
   isLoaded = false;
-  files: FileInfo[];
+  allFiles: FileInfo[];
+  currentFiles=  new Array();
   currentFolder = '';
   currentFolders = new Set();
   currentLevel = 0;
   ngOnInit(): void {
     this.fileService.getFiles().subscribe({
       next:(result) =>{
-          this.files = result;
+          this.allFiles = result;
           this.isLoaded = true;
-          for(let f of this.files){
+          for(let f of this.allFiles){
             if(f.folderName !== ''){
               this.currentFolders.add(f.folderName.split('/')[0].toString());
+            }
+          }
+          for(let f of this.allFiles){
+            if(f.folderName === ''){
+              this.currentFiles.push(f);
+
             }
           }
           console.log(this.currentFolders);
       },
       error:(err) =>{
-
-        
       }
     })
     
   }
 
+  public redisplayItemsAndFolder(folder: string): void { 
+      let new_files =  new Array();
+      this.currentFolder += folder+'/';
+      for(let f of this.allFiles){
+        if(f.folderName===folder){
+          new_files.push(f);
+        }
+      }
+      this.currentLevel += 1;
+      this.currentFiles = new_files;
+      this.currentFolders.clear();
+      for(let f of this.allFiles){
+        // slicice/moje/sdadas/fsafsa
+        if(f.folderName !== '' && f.folderName.includes("/")){
+          let value = f.folderName.split('/')[this.currentLevel].toString();
+          let allSplit = f.folderName.split('/')
+          let currentLevelInThisFolder = '';
+          for(let i = 0;i<this.currentLevel;i++){
+            currentLevelInThisFolder += allSplit[i].toString() + '/';
+          }
+          if(currentLevelInThisFolder === this.currentFolder){
+            this.currentFolders.add(value);
+
+          }
+          
+        }
+      }
+  }
+  
   public constructor(private fileService: FileService){
 
   }
-
-  public redisplayItemsAndFolder(folder: string): void {
-    alert(folder);
-  }
-  
 
 }
