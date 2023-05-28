27e894c5211f50a87be78dc7eeb0fac49dddebc3
nebojsavuga <vuga.sv53.2020@uns.ac.rs>
2023-05-28 17:26:20 +0200

    Working

--- cloud/src/app/layout/files-display/files-display.component.ts
+++ cloud/src/app/layout/files-display/files-display.component.ts
@@ -31,7 +31,6 @@ export class FilesDisplayComponent implements OnInit{
 
             }
           }
-          console.log(this.currentFolders);
       },
       error:(err) =>{
       }
@@ -42,12 +41,13 @@ export class FilesDisplayComponent implements OnInit{
   public redisplayItemsAndFolder(folder: string): void { 
       
       let new_files =  new Array();
-      this.currentFolder += folder+'/';
+      this.currentFolder += folder;
       for(let f of this.allFiles){
-        if(f.folderName===folder){
+        if(f.folderName===this.currentFolder){
           new_files.push(f);
         }
       }
+      this.currentFolder += '/';
       this.currentLevel += 1;
       this.currentFiles = new_files;
       this.currentFolders.clear();
@@ -59,9 +59,13 @@ export class FilesDisplayComponent implements OnInit{
           let allSplit = f.folderName.split('/');
           let currentLevelInThisFolder = '';
           for(let i = 0;i<this.currentLevel;i++){
+            if(i != this.currentLevel - 1)
             currentLevelInThisFolder += allSplit[i].toString() + '/';
+          else
+            currentLevelInThisFolder += allSplit[i].toString();
+            
           }
-          if(currentLevelInThisFolder === this.currentFolder){
+          if(currentLevelInThisFolder + '/' === this.currentFolder){
             this.currentFolders.add(value);
 
           }
@@ -72,49 +76,58 @@ export class FilesDisplayComponent implements OnInit{
   
   goBackFolder(){
 
-    if(this.currentFolder === '')return;
-    this.currentLevel -=1;
-    let currentLevelInThisFolder = '';
-    if(this.currentLevel == 0) this.currentFolder = '';
-
-    else{
-      let oldFolders = this.currentFolder.split('/');
-      for(let i =0;i<this.currentLevel;i++){
-        currentLevelInThisFolder += oldFolders[i].toString() + '/';
-
-      }
-      
-    }
-    this.currentFolder = currentLevelInThisFolder;
-    let new_files =  new Array();
-    for(let f of this.allFiles){
-      
-      if(f.folderName===this.currentFolder){
-        new_files.push(f);
-      }
-    }
-
-    this.currentFiles = new_files;
-    this.currentFolders.clear();
-
-    for(let f of this.allFiles){
-      // slicice/moje/sdadas/fsafsa
-      if(f.folderName !== ''){
-        let value = f.folderName.split('/')[this.currentLevel].toString();
-        let allSplit = f.folderName.split('/')
-        let currentLevelInThisFolder = '';
-        for(let i = 0;i<this.currentLevel;i++){
-          currentLevelInThisFolder += allSplit[i].toString() + '/';
-        }
-        if(currentLevelInThisFolder === this.currentFolder){
-          this.currentFolders.add(value);
-
-        }
-        
-      }
-    }
-      
+    if(this.currentFolder ==='') return;
+    this.currentLevel -= 1;
     
+    //slike/slash/ - > slike/
+
+    let newCurrentFolder = '';
+    let folders = this.currentFolder.split('/');
+    for(let i =0;i<this.currentLevel;i++){
+        newCurrentFolder += folders[i] + '/';
+    }
+    this.currentFolder = newCurrentFolder;
+    this.currentFiles.length = 0;
+    if(this.currentLevel == 0){
+      for(let f of this.allFiles){
+        if(f.folderName ===this.currentFolder){
+            this.currentFiles.push(f);
+        }
+      }
+
+
+
+    }else{
+      for(let f of this.allFiles){
+        if(f.folderName + '/'===this.currentFolder){
+            this.currentFiles.push(f);
+        }
+      }
+
+    }
+    this.currentFolders.clear();
+    let targetChar = '/';
+    for(let f of this.allFiles){
+        if(f.folderName !== '' && f.folderName.includes('/') && this.currentLevel != 0){
+          let lastIndex = f.folderName.lastIndexOf(targetChar);
+          if(lastIndex != -1){
+            this.currentFolders.add(f.folderName.substring(lastIndex + 1, f.folderName.length));
+          }
+         
+        }if(f.folderName !== '' && !f.folderName.includes('/') && this.currentLevel == 0){
+          this.currentFolders.add(f.folderName);
+        }
+      
+    }
+
+  }
+
+  uploadToCurrentFolder(): void{
+
+  }
+
+  showFile(file: FileInfo):void{
+
   }
 
   public constructor(private fileService: FileService){
