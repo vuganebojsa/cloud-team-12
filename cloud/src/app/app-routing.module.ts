c48a943e47de0c316f7b8165822038c44eec5d8b
Nebojša Vuga <72966399+nebojsavuga@users.noreply.github.com>
2023-06-12 12:59:52 +0200

    Added routing for shared files

--- cloud/src/app/app-routing.module.ts
+++ cloud/src/app/app-routing.module.ts
@@ -5,12 +5,16 @@ import { LoginComponent } from './layout/login/login.component';
 import { UploadFileComponent } from './layout/upload-file/upload-file.component';
 import { EnterCodeComponent } from './layout/enter-code/enter-code.component';
 import { HomeComponent } from './layout/home/home.component';
+import { ShareFileToOtherComponent } from './layout/share/share-file-to-other/share-file-to-other.component';
+import { SharedFilesComponent } from './layout/share/shared-files/shared-files.component';
 
 const routes: Routes = [
   {path:'register', component:RegisterComponent},
   {path:'login', component:LoginComponent},
   {path:'upload-file', component:UploadFileComponent},
   {path:'verify', component:EnterCodeComponent},
+  {path:'share-file', component:ShareFileToOtherComponent},
+  {path:'shared-files', component:SharedFilesComponent},
   {path:'**', component:HomeComponent},
   {path:'', component:HomeComponent},
   {path:'', component:HomeComponent},
