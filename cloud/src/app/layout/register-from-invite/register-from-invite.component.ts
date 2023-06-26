b4e107d86fb05a8bafa4bf781408e62163fa6085
nebojsavuga <vuga.sv53.2020@uns.ac.rs>
2023-06-26 18:56:33 +0200

    Slight modificaiton

--- cloud/src/app/layout/register-from-invite/register-from-invite.component.ts
+++ cloud/src/app/layout/register-from-invite/register-from-invite.component.ts
@@ -18,7 +18,7 @@ export class RegisterFromInviteComponent {
     password: new FormControl('', [Validators.required, Validators.minLength(10)]),
     birthDate: new FormControl('', [Validators.required]),
     username: new FormControl('', [Validators.required, Validators.minLength(3)]),
-    inviteUsername: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    inviterEmail: new FormControl('', [Validators.required, Validators.minLength(3)]),
   });
 
   constructor(private router: Router, private authenticationService: AuthenticationService)
@@ -33,7 +33,7 @@ export class RegisterFromInviteComponent {
       password: this.registerForm.value.password,
       birthDate:this.registerForm.value.birthDate,
       username:this.registerForm.value.username,
-      inviterUsername: this.registerForm.value.inviteUsername
+      inviterEmail: this.registerForm.value.inviterEmail
     };
     if(user.username.includes('-')){
       alert('Invalid username. Please remove the character -.');
